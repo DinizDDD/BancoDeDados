294827e5bad0da1d4258c389c9c047472abf0d2c
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-05-20 09:33:59 -0300

    update modulo 3

---
+++ modulo3.sql
@@ -0,0 +1,7 @@
+SELECT * from livro WHERE ano_publicacao > 2015;
+
+SELECT * from autor WHERE nacionalidade = "Brasil";
+
+SELECT * from cliente WHERE cidade LIKE "S%";
+
+SELECT * from livro WHERE preco BETWEEN 30.00 AND 50.00;
