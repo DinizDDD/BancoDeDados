1b7e92c2bf3e661f0e77b59e0e7118e924f9da7e
yDewolf <davidinizdalevedo9@gmail.com>
2025-06-05 11:29:26 -0300

    make modulo 5

--- modulo5.sql
+++ modulo5.sql
@@ -1,1 +1,25 @@
-SELECT * FROM livros WHERE 
+SELECT 
+    livro.titulo,
+    autor.nome AS nome_autor
+FROM livro
+JOIN autor ON livro.id_autor = autor.id_autor;
+
+SELECT 
+    cliente.nome AS nome_cliente,
+    livro.titulo AS titulo_livro,
+    venda.data_venda
+FROM venda
+JOIN cliente ON venda.id_cliente = cliente.id_cliente
+JOIN livro ON venda.id_livro = livro.id_livro;
+
+SELECT DISTINCT 
+    cliente.nome
+FROM venda
+JOIN cliente ON venda.id_cliente = cliente.id_cliente
+JOIN livro ON venda.id_livro = livro.id_livro
+JOIN autor ON livro.id_autor = autor.id_autor
+WHERE autor.nacionalidade <> 'Brasil';
+
+
+
+
